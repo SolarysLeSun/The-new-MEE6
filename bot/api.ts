a1a3b64572fbb46e7898c19584a42c5323e8cac5
Enzo prados <enzo.prados@gmail.com>
2025-09-19 19:49:04 +0000

    sdf

--- bot/api.ts
+++ bot/api.ts
@@ -10,7 +10,7 @@ import { updateGuildCommands } from './handlers/commandHandler';
 import { generateKeywords } from '@/ai/flows/keyword-generation-flow';
 import { knowledgeCreationFlow } from '@/ai/flows/knowledge-creation-flow';
 
-const API_PORT = process.env.BOT_API_PORT || 25875;
+const API_PORT = process.env.BOT_API_PORT || 10033;
 
 export function startApi(client: Client) {
     const app = express();
