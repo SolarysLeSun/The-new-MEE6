c2af22f4a8ab4387f760417edc3e511595f7e34a
Enzo prados <enzo.prados@gmail.com>
2025-10-04 15:57:54 +0000

    changement port api bot

--- bot/api.ts
+++ bot/api.ts
@@ -10,7 +10,7 @@ import { updateGuildCommands } from './handlers/commandHandler';
 import { generateKeywords } from '@/ai/flows/keyword-generation-flow';
 import { knowledgeCreationFlow } from '@/ai/flows/knowledge-creation-flow';
 
-const API_PORT = process.env.BOT_API_PORT || 3001;
+const API_PORT = process.env.BOT_API_PORT || 3630;
 
 export function startApi(client: Client) {
     const app = express();
