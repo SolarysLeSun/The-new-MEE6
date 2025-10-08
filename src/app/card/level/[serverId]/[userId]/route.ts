ee7b5b5786660ff5393a24e43c5c9690d9d8d8c6
Enzo prados <enzo.prados@gmail.com>
2025-10-08 12:50:30 +0000

    Okey, parfait ! juste un dernier truc, les personnes qui on des nom avec

--- src/app/card/level/[serverId]/[userId]/route.ts
+++ src/app/card/level/[serverId]/[userId]/route.ts
@@ -25,7 +25,7 @@ export async function GET(req: NextRequest, { params }: { params: { serverId: st
     const rank = parseInt(searchParams.get('rank') || '0', 10);
     const barColor = searchParams.get('barColor') || '#e597c4' // A nice pink from your template
     const textColor = searchParams.get('textColor') || '#e597c4'
-    const backgroundUrl = searchParams.get('backgroundUrl');
+    const backgroundUrl = searchParams.get('backgroundUrl') || 'https://nightproject.nationquest.fr/levelbw.jpg';
 
 
     // 1. Création du canvas
