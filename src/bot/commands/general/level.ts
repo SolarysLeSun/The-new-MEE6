5c9b59582709a28f4990d92cf34e68f7ff3f023f
Enzo prados <enzo.prados@gmail.com>
2025-10-08 11:23:48 +0000

    Depuis les dernières versions de Next.js (v14/v15 avec App Router), le c

--- src/bot/commands/general/level.ts
+++ src/bot/commands/general/level.ts
@@ -39,7 +39,7 @@ const LevelCommand: Command = {
             const levelInfo = getUserLevel(targetUser.id, interaction.guild.id);
             const rank = getUserRank(targetUser.id, interaction.guild.id);
             
-            const cardUrl = new URL(`${process.env.PANEL_BASE_URL}/card/level/${interaction.guild.id}/${targetUser.id}`);
+            const cardUrl = new URL(`${process.env.PANEL_BASE_URL}/api/card/level/${interaction.guild.id}/${targetUser.id}`);
             cardUrl.searchParams.append('displayName', member.displayName);
             cardUrl.searchParams.append('avatarUrl', targetUser.displayAvatarURL({ extension: 'png', size: 256 }));
             cardUrl.searchParams.append('level', levelInfo.level.toString());
